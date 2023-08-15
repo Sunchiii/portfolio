17ee4d4334a76aaa77606050e860132281d1278c
Sunchii <rachanlamoolphal@gmail.com>
2023-08-15 18:59:17 +0700

    add param to article card

--- src/app/components/ArticleCard.jsx
+++ src/app/components/ArticleCard.jsx
@@ -1,19 +1,20 @@
 "use client"
 import Image from 'next/image'
 import Link from 'next/link'
+import moment from 'moment'
 
-export default function ArticleCard(){
+export default function ArticleCard({title,description, created_at, article_type}){
   return (
     <div className="w-[280px] relative m-5">
         <div className='w-full h-40 overflow-hidden'>
         <Image className='object-cover' src="/assets/images/sunchi.jpeg" width={500} height={500} />
       </div>
       <div className='bg-[white] text-[black] pb-20 p-3'>
-        <p className='text-xs '>March 20 2015</p>
-        <h1 className='text-xl font-bold'>What happened in Thailand?</h1>
-        <p className='text-sm fifty-words'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+        <p className='text-xs '>{moment(created_at).format("MMM Do YY")}</p>
+        <h1 className='text-xl font-bold'>{title}</h1>
+        <p className='text-sm fifty-words'>{description}</p>
 
-        <h1 className='absolute top-3 left-3 text-[#f7c618] bg-[white] p-1'>Tech</h1>
+        <h1 className='absolute top-3 left-3 text-[#f7c618] bg-[white] p-1'>{article_type}</h1>
         <Link className='text-[#f7c618] absolute bottom-2 left-3' href="/">click to read more..</Link>
       </div>
     </div>
