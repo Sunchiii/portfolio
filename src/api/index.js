4db6bd17e86fbc211f4c2fa278e948d4d67b0ec1
Sunchii <rachanlamoolphal@gmail.com>
2023-08-16 19:29:40 +0700

    request data by pagination

--- src/api/index.js
+++ src/api/index.js
@@ -1,19 +1,4 @@
-import { useCallback } from "react"
-import {useSearchParams, useRouter, usePathname} from "next/navigation"
 
-const searchParams = useSearchParams()
-const router = useRouter()
-const pathname = usePathname()
 
 export const API_URL = "http://localhost:8080/v1";
 
-
-export const createQueryString = useCallback(
-    (name, value) => {
-      const params = new URLSearchParams(searchParams)
-      params.set(name, value)
- 
-      return params.toString()
-    },
-    [searchParams]
-  )
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,11 @@
 "use client"
-import { useEffect, useState } from "react"
+import { useEffect, useState,useCallback } from "react"
 import CategorySelectTab from "./components/categoryTab"
 import SearchBar from "./components/serachBar"
 import Link from 'next/link'
 import ArticleCard from "./components/ArticleCard"
 import { GetArticles } from "@/api/article"
 import {useSearchParams, useRouter, usePathname} from "next/navigation"
-import { createQueryString } from "@/api"
 
 export default function About() {
   const searchParams = useSearchParams()
@@ -15,13 +14,27 @@ export default function About() {
   const [data, setData] = useState([])
   const [initLoading, setInitLoading] = useState(true)
 
+  const createQueryString = useCallback(
+    (name, value) => {
+      const params = new URLSearchParams()
+      // clear old params
+      params.delete(name)
+      // set new params
+      params.set(name, value)
+
+      // return new params
+      return params.toString()
+    },
+    [searchParams]
+  )
   async function CallApi() {
-    const res = await GetArticles(10,1)
+    const res = await GetArticles(parseInt(searchParams.get("limit")),parseInt(searchParams.get("page")))
     setData(res?.articles)
     setInitLoading(false)
   }
   useEffect(() => {
     CallApi()
+    console.log(pathname)
   }, [])
   return (
     <div style={{ "paddingTop": '50px', }}>
@@ -31,6 +44,7 @@ export default function About() {
         <CategorySelectTab />
         <div className="flex flex-wrap" style={{ display: initLoading ? "none" : "flex" }}>
           {
+            data?.length === 0 ? <div className="h-[500px] w-full text-center pt-24">has no data in this page</div>:
             data?.map(e=> (
               <Link href={`/article/${e?.id}`} key={e?.id}>
                 <ArticleCard image_exam={e?.image_exam} title={e?.title} description={e?.description} created_at={e?.created_at} article_type={e?.article_type} />
@@ -47,9 +61,11 @@ export default function About() {
           <div className="join inline">
             <button className="join-item btn">«</button>
             <button onClick={()=>{
-              router.push(pathname + "?"+createQueryString("page","1")+createQueryString("limit","10"))
+              router.push("?"+createQueryString("page","1")+"&"+createQueryString("limit","10"))
             }} className="join-item btn">1</button>
-            <button className="join-item btn">2</button>
+            <button onClick={()=>{
+              router.push("?"+createQueryString("page","2")+"&"+createQueryString("limit","10"))
+            }} className="join-item btn">2</button>
             <button className="join-item btn">3</button>
             <button className="join-item btn">»</button>
           </div>
