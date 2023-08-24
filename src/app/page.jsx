772df7cd09c5920ca8f56919848ba4779593b6c7
Sunchii <rachanlamoolphal@gmail.com>
2023-08-24 19:33:38 +0700

    finish pagenation

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,17 @@
 "use client"
-import { useEffect, useState,useCallback } from "react"
+import { useEffect, useState, useCallback } from "react"
 import CategorySelectTab from "./components/categoryTab"
 import SearchBar from "./components/serachBar"
 import Link from 'next/link'
 import ArticleCard from "./components/ArticleCard"
 import { GetArticles } from "./api/article"
-import {useSearchParams, useRouter} from "next/navigation"
+import { useSearchParams, useRouter } from "next/navigation"
 
 export default function About() {
   const searchParams = useSearchParams()
   const router = useRouter()
   const [data, setData] = useState([])
-  const [allData,setAllData] = useState({articles:[],meta:{}})
+  const [allData, setAllData] = useState({ articles: [], meta: {} })
   const [initLoading, setInitLoading] = useState(true)
 
   const createQueryString = useCallback(
@@ -30,21 +30,22 @@ export default function About() {
   async function CallApi() {
     let limit = searchParams.get("limit")
     let page = searchParams.get("page")
-    const res = await GetArticles(parseInt(limit),parseInt(page))
+    const res = await GetArticles(parseInt(limit), parseInt(page))
     setAllData(res)
     setInitLoading(false)
   }
-  async function PageCallApi(_page,_limit) {
-    const res = await GetArticles(_limit,_page)
+  async function PageCallApi(_page, _limit) {
+    setInitLoading(true)
+    const res = await GetArticles(_limit, _page)
     setAllData(res)
     setInitLoading(false)
   }
   useEffect(() => {
     CallApi()
   }, [])
-  useEffect(()=>{
+  useEffect(() => {
     setData(allData.articles)
-  },[allData])
+  }, [allData])
   return (
     <div style={{ "paddingTop": '50px', }}>
       <div className="container">
@@ -53,12 +54,12 @@ export default function About() {
         <CategorySelectTab />
         {data && <div className="flex flex-wrap" style={{ display: initLoading ? "none" : "flex" }}>
           {
-            data?.length === 0 ? <div className="h-[500px] w-full text-center pt-24">has no data in this page</div>:
-            data?.map(e=> (
-              <Link href={`/article/${e?.id}`} key={e?.id}>
-                <ArticleCard image_exam={e?.image_exam} title={e?.title} description={e?.description} created_at={e?.created_at} article_type={e?.article_type} />
-              </Link>
-            ))
+            data?.length === 0 ? <div className="h-[500px] w-full text-center pt-24">has no data in this page</div> :
+              data?.map(e => (
+                <Link href={`/article/${e?.id}`} key={e?.id}>
+                  <ArticleCard image_exam={e?.image_exam} title={e?.title} description={e?.description} created_at={e?.created_at} article_type={e?.article_type} />
+                </Link>
+              ))
           }
         </div>}
         <div style={{
@@ -68,17 +69,38 @@ export default function About() {
         </div>
         <div className="flex justify-center py-5">
           <div className="join inline">
-            <button className="join-item btn">«</button>
+            <button onClick={()=>{
+              router.push("?" + createQueryString("page", 1) + "&" + createQueryString("limit", "10"))
+              PageCallApi(1, 10)
+            }} className="join-item btn">First page</button>
             {
               allData &&
-              Array.from({length:allData?.meta?.page_count}, (_, i) => i + 1).map((p)=>(
-                <button key={p} onClick={()=>{
-                  router.push("?"+createQueryString("page",p)+"&"+createQueryString("limit","10"))
-                  PageCallApi(p,10)
-                }} className="join-item btn">{p}</button>
-              ))
+              Array.from({ length: allData?.meta?.page_count }, (_, i) => i + 1).map((p) => {
+                if (p >= 1 && p <= 3) {
+                  return (
+                    <button style={{opacity: p == searchParams.get("page") ? 0.7 : 1}} key={p} onClick={() => {
+                      router.push("?" + createQueryString("page", p) + "&" + createQueryString("limit", "10"))
+                      PageCallApi(p, 10)
+                    }} className="join-item btn">{p}</button>
+                  )
+                }
+                else if (p >= length-3 && p <= length){
+                  return (
+                    <button style={{opacity: p == searchParams.get("page") ? 0.7 : 1}} key={p} onClick={() => {
+                      router.push("?" + createQueryString("page", p) + "&" + createQueryString("limit", "10"))
+                      PageCallApi(p, 10)
+                    }} className="join-item btn">{p}</button>
+                  )
+                }
+                else{
+                  return
+                }
+              })
             }
-            <button className="join-item btn">»</button>
+            <button onClick={()=>{
+              router.push("?" + createQueryString("page", allData?.meta?.page_count) + "&" + createQueryString("limit", "10"))
+              PageCallApi(allData?.meta?.page_count, 10)
+            }} className="join-item btn">Last page</button>
           </div>
         </div>
       </div>
